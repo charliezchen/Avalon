a459fe05af70b8dfd8067a661fd62b06a4d59e16
Jinli Xiao <jinli.xiao@nyu.edu>
2024-01-12 15:42:23 -0500

    enable show/hide identity

--- client/src/components/Avalon.vue
+++ client/src/components/Avalon.vue
@@ -2,32 +2,30 @@
   <div>
     <span>Avalon Game Helper</span>
 
+    <!-- Section for joining the game -->
     <div v-if="!name_selected">
       <p>Name:</p>
       <input type="text" v-model="name" />
       <button v-on:click="join(name)">Create/Join</button>
     </div>
 
+    <!-- Display the number of players -->
     <p>Number of players: {{ num_people }}</p>
     <ul>
-      <li v-for="u in all_users" v-bind:key="u.userID">{{ u.name }}</li>
+      <li v-for="user in all_users" :key="user.userID">{{ user.name }}</li>
     </ul>
 
+    <!-- Game actions and information after joining -->
     <div v-if="name_selected">
       <p v-if="is_host && all_users.length < 5">You are the host</p>
-      <p v-if="all_users.length < 5">
-        Waiting for at least 5 players to join ...
-      </p>
-      <p v-if="all_users.length >= 5 && !identity.identity">
-        Waiting for {{ all_users[0].name }} to start the game ...
-      </p>
-      <button v-on:click="start()" v-if="is_host && all_users.length >= 5">
-        Start game
-      </button>
-      <p v-if="identity.identity">Identity: {{ identity.identity }}</p>
-      <p v-if="identity.identity">
-        People you can see: {{ identity.viewerContext }}
-      </p>
+      <p v-if="all_users.length < 5">Waiting for at least 5 players to join ...</p>
+      <p v-if="all_users.length >= 5 && !identity.identity">Waiting for {{ all_users[0].name }} to start the game ...</p>
+      <button v-if="is_host && all_users.length >= 5" v-on:click="start()">Start game</button>
+      <div v-if="identity.identity">
+        <button @click="toggleIdentity">Show/Hide Identity</button>
+        <p v-if="showIdentity">Identity: {{ identity.identity }}</p>
+        <p v-if="showIdentity">People you can see: {{ identity.viewerContext }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -35,60 +33,33 @@
 <script>
 import io from "socket.io-client";
 
-const room = io("http://localhost:3000", {
-  autoConnect: false,
-});
+// Socket.io connection setup
+const room = io("http://localhost:3000", { autoConnect: false });
 room.connect();
-const game = io("http://localhost:3000/game", {
-  autoConnect: false,
-});
+const game = io("http://localhost:3000/game", { autoConnect: false });
 
-// For debug
-game.onAny((event, ...args) => {
-  console.log(event, args);
-});
-room.onAny((event, ...args) => {
-  console.log(event, args);
-});
+// Debugging: Log all socket events
+game.onAny((event, ...args) => console.log(event, args));
+room.onAny((event, ...args) => console.log(event, args));
 
 export default {
   name: "Avalon",
   data() {
     return {
       name: "",
-      identity: {
-        identity: "",
-        viewerContext: [],
-      },
-      game: game,
-      room: room,
+      identity: { identity: "", viewerContext: [] },
+      game,
+      room,
       name_selected: false,
       all_users: [],
       is_host: false,
+      showIdentity: true,
     };
   },
   created() {
-    this.room.on("name", (name) => {
-      this.name = name;
-      this.name_selected = true;
-    });
-    this.room.on("users", (data) => {
-      this.all_users = data;
-      if (this.all_users.length > 0 && this.all_users[0].name == this.name) {
-        this.is_host = true;
-      }
-      console.log(data);
-    });
-    this.game.on("identity", (identity) => {
-      console.log("receive identity", identity);
-      this.identity = identity;
-      this.identity.viewerContext = this.identity.viewerContext.map(
-        (id) => this.all_users[id].name
-      );
-    });
+    this.setupSocketListeners();
   },
-  mounted() {
-  },
+  mounted() {},
   destroyed() {
     this.room.off("connect_error");
     this.game.off("connect_error");
@@ -99,19 +70,36 @@ export default {
     },
   },
   methods: {
+    setupSocketListeners() {
+      this.room.on("name", name => {
+        this.name = name;
+        this.name_selected = true;
+      });
+      this.room.on("users", data => {
+        this.all_users = data;
+        this.is_host = this.all_users.length > 0 && this.all_users[0].name === this.name;
+      });
+      this.game.on("identity", identity => {
+        this.identity = identity;
+        this.identity.viewerContext = this.identity.viewerContext.map(id => this.all_users[id].name);
+      });
+    },
     join(name) {
       this.name_selected = true;
       this.room.emit("join", name);
       this.game.connect();
-      console.log("name", name);
     },
     start() {
-      console.log("start");
       this.game.emit("start", 1);
+    },
+    toggleIdentity() {
+      this.showIdentity = !this.showIdentity;
     },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<!-- Placeholder for future scoped CSS -->
+<style scoped>
+/* Styles specific to this component */
+</style>
